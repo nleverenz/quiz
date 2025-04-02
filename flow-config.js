eaceb02682f6ff43ae09e6dc4a130f4a9d3c0c6a
nleverenz <70814016+nleverenz@users.noreply.github.com>
2025-04-02 18:21:39 -0400

    update

--- flow-config.js
+++ flow-config.js
@@ -1,7 +1,7 @@
 const flow = [
   {
     type: "figma",
-    url: "https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/proto/YOUR_FILE",
+    url: "https://embed.figma.com/proto/KP4vCoEMomjiCij04nbjGF/Counting--Copy-?page-id=2001%3A38&node-id=2001-190&viewport=134%2C297%2C0.08&scaling=scale-down&content-scaling=fixed&starting-point-node-id=2001%3A190&embed-host=share",
     audio: "audio/question1.mp3"
   },
   {
@@ -10,7 +10,7 @@ const flow = [
   },
   {
     type: "figma",
-    url: "https://www.figma.com/embed?embed_host=share&url=https://www.figma.com/proto/YOUR_FILE_2",
+    url: "https://embed.figma.com/proto/KP4vCoEMomjiCij04nbjGF/Counting--Copy-?page-id=2001%3A38&node-id=2001-190&viewport=134%2C297%2C0.08&scaling=scale-down&content-scaling=fixed&starting-point-node-id=2001%3A190&embed-host=share",
     audio: "audio/question2.mp3"
   }
 ];
