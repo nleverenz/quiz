30b4139ea5c38558d3a5accb27bdf367a36e9288
nleverenz <70814016+nleverenz@users.noreply.github.com>
2025-04-02 18:25:57 -0400

    push

--- flow-config.js
+++ flow-config.js
@@ -2,7 +2,7 @@ const flow = [
   {
     type: "figma",
     url: "https://embed.figma.com/proto/KP4vCoEMomjiCij04nbjGF/Counting--Copy-?page-id=2001%3A38&node-id=2001-190&viewport=134%2C297%2C0.08&scaling=scale-down&content-scaling=fixed&starting-point-node-id=2001%3A190&embed-host=share",
-    audio: "audio/question1.mp3"
+    // audio: "audio/question1.mp3" // optional: uncomment once the file exists
   },
   {
     type: "video",
@@ -11,6 +11,6 @@ const flow = [
   {
     type: "figma",
     url: "https://embed.figma.com/proto/KP4vCoEMomjiCij04nbjGF/Counting--Copy-?page-id=2001%3A38&node-id=2001-190&viewport=134%2C297%2C0.08&scaling=scale-down&content-scaling=fixed&starting-point-node-id=2001%3A190&embed-host=share",
-    audio: "audio/question2.mp3"
+    // audio: "audio/question2.mp3" // optional: uncomment once the file exists
   }
 ];
